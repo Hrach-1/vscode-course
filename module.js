1b86f38e526e795a9a2babff060afaad0ad27d07
Hrach-1 <steepgaskin@gmail.com>
2021-01-22 14:56:24 +0400

    start

---
+++ module.js
@@ -0,0 +1,18 @@
+alert('Module')
+if (true) {
+  alert(true)
+}
+
+class Car {
+  constructor() {
+    super()
+  }
+
+  get() {
+    setTimeout(() => {
+      const obj = {
+        a: 1,
+      }
+    })
+  }
+}
